3ee85cad7021deffe267a99642d122911ac3748a
Luca Bacchi <bacchilu@gmail.com>
2011-10-06 14:23:21 +0200

    Some minor refactoring

--- tsvparse.py
+++ tsvparse.py
@@ -2,21 +2,12 @@ from urlparse import urlparse
 import sys
 
 
-def lines(fp):
-    return (l for i, l in enumerate(fp) if i > 1)
-
-
-def urls(fp):
-    return (l.split('\t')[1][1:-1] for l in lines(fp) if l.split('\t')[1])
-
-
-def domains(fp):
-    return (urlparse(url).netloc for url in urls(fp))
-
-
 def getEpuratedDomains(fileName):
     with open(fileName, 'r') as fp:
-        return list(set(domains(fp)))
+        lines = (l for i, l in enumerate(fp) if i > 1)
+        urls = (l.split('\t')[1][1:-1] for l in lines if l.split('\t')[1])
+        domains = (urlparse(url).netloc for url in urls)
+        return list(set(domains))
 
 
 if __name__ == '__main__':
